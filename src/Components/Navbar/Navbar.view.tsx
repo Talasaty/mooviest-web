43ad30a399499af712c8ffc374a0f8d99ef342a2
jmrojas <jmrojas@ingenia.es>
2018-09-11 13:57:32 +0200

    ADD: Formik

---
+++ src/Components/Navbar/Navbar.view.tsx
@@ -0,0 +1,15 @@
+import * as React from "react";
+import styled from "styled-components";
+
+
+class Navbar extends React.Component {
+  public render() {
+    return (
+      <div className="Navbar-wrapper">
+      <span>Aquí el menu</span>
+      </div>
+    );
+  }
+}
+
+export default Navbar;
