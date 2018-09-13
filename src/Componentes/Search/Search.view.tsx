71e5bbb1f5a3e1281aa1022a6e22f90ef79430b0
Juan Luis Mayorga Navarrete <jlmayorga@ingenia.local>
2018-09-13 11:38:50 +0200

    ADD: axios y peticiones get al buscador

--- src/Componentes/Search/Search.view.tsx
+++ src/Componentes/Search/Search.view.tsx
@@ -14,7 +14,7 @@ const FieldStyled = styled(Field)<{word: string}>`
     padding: 5px;
     width: 20px;
     outline: none;
-    -webkit-transition: width .2s;
+    -webkit-transition: width .3s;
     min-width: 20px;
     width: ${p => p.word === "true" ? '200px' : '20px'};
 
@@ -39,46 +39,106 @@ const Button = styled.button`
 
 interface Values {
     keyWord: string;
-}
-
-const InnerForm = (props: FormikProps<Values>) => {
-    const { touched, errors } = props
-    console.log(props.values)
-    
-    return(
-        <Form>
-            <FieldStyled type="keyWord" name="keyWord" word={props.values.keyWord !== "" ? "true" : "false"}/>
-            { touched.keyWord && errors.keyWord && <div>{errors.keyWord}</div> }
-            <Button type="submit">
-            >
-            </Button>
-        </Form>
-    )
+    dataResult: object;
+    results: object;
 }
 
 interface MyValues {
     initialKeyWord?: string;
+    initialDataResult?: object;
+    initialResults?: object;
 }
 
-const MyForm = withFormik<MyValues, Values>({
-    mapPropsToValues: props => {
-        return {
-            keyWord: props.initialKeyWord || '',
-        }
-    },
-    handleSubmit: values => {
-        console.log('values:', values)
-        console.log(getData)
-    },
-})(InnerForm)
-    
+class Search extends React.Component<MyValues, Values> {
 
-const Search = () => {
-    return (
-        <div>
-            <MyForm/>
-        </div>
-    )
+    state = {
+        keyWord: "",
+        initialKeyWord: "",
+        dataResult: {
+                        page: 0,
+                        results: [],
+                        total_pages: 0,
+                    },
+        results: {
+            adult: false,
+            backdrop_path: "",
+            genre_ids: [],
+            id: 0,
+            original_language: "",
+            original_title: "",
+            overview: "",
+            popularity: 0,
+            poster_path: "",
+            release_date: "",
+            title: "",
+            video: false,
+            vote_average: 0,
+            vote_count: 0,
+        }
+    }
+
+
+    InnerForm = (props: FormikProps<Values>) => {
+        const { touched, errors } = props
+        return(
+            <Form>
+                <FieldStyled type="keyWord" name="keyWord" word={props.values.keyWord !== "" ? "true" : "false"}/>
+                { touched.keyWord && errors.keyWord && <div>{errors.keyWord}</div> }
+                <Button type="submit">
+                >
+                </Button>
+            </Form>
+        )
+    }
+
+    MyForm = withFormik<MyValues, Values>({
+        mapPropsToValues: props => {
+            return {
+                keyWord: props.initialKeyWord || '',
+                dataResult: props.initialDataResult || {
+                                                        page: 0,
+                                                        results: [],
+                                                        total_pages: 0,
+                                                    },
+                results: props.initialResults || {
+                                                        adult: false,
+                                                        backdrop_path: "",
+                                                        genre_ids: [],
+                                                        id: 0,
+                                                        original_language: "",
+                                                        original_title: "",
+                                                        overview: "",
+                                                        popularity: 0,
+                                                        poster_path: "",
+                                                        release_date: "",
+                                                        title: "",
+                                                        video: false,
+                                                        vote_average: 0,
+                                                        vote_count: 0,
+                                                }
+            }
+        },
+        handleSubmit: values => {
+            console.log('values:', values)
+            let keyWords = values.keyWord.replace(" ","+")
+            getData(keyWords).then((response) => { this.setState({dataResult: response, results: response.results})})
+                     .catch((err) => {console.log('Se ha produccido el error: ',err)})
+            
+            
+        },
+    })(this.InnerForm)
+
+
+    render() {
+        console.log('Resutlado de la búsqueda: ',this.state.dataResult)
+        console.log('Results: ', this.state.results)
+        return(
+            <div>
+                <this.MyForm/>
+            <br/>
+            </div>
+        )
+    }
 }
 
 export default Search
